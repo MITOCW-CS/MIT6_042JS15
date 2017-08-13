9ea1bc2ceaad41baec525827a12ac3f3fc76e080
mitocw-cs <mitocw.cs@gmail.com>
2017-08-13 08:29:06 +0700

    MIT6_042JS15

--- DataTypes/sets/ToDo.py
+++
@@ -1,7 +0,0 @@
-"""
-
-Software Dev-Process: Planning, Processing, Debugging, Testing
-                                ||
-                                ||
-Products Security (Software Security): Bug, Zero-days, Exploit
-"""
